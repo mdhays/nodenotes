79b1fc84359ad448b25387406d35e92cf8ab9d05
Matt Hays <mdhays1995@gmail.com>
2016-02-18 10:36:55 -0600

    Added form.

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 'use strict';
 
+const bodyParser = require('body-parser');
 const express = require('express');
-
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.get('/', (req, res) => {
   res.send('server route');
 });
 
+app.get('/notes/new', (req, res) => {
+  res.render('new-note');
+});
+
+app.post('/notes', (req, res) => {
+  res.redirect('/');
+});
 
 app.listen(3000, () => {
   console.log(`nodenotes server running on port: ${port}`)
