ef6b08e6cf6e26ee0d9b40d70b023b1a62c6c3b9
Matt Hays <mdhays1995@gmail.com>
2016-02-18 11:47:22 -0600

    Refactored code into seperate modules.

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 
+const note = require('./routes/note');
+
 const app = express();
 const port = process.env.PORT || 3000;
-const Note = mongoose.model('notes', mongoose.Schema({
-  title: String,
-  text: String
-}));
+const Note = require('./models/note');
+const router = express.Router();
 
 app.set('view engine', 'jade');
 
@@ -21,25 +21,7 @@ app.get('/', (req, res) => {
   res.send('server route');
 });
 
-app.get('/notes/new', (req, res) => {
-  res.render('new-note');
-});
-
-app.get('/notes/:id', (req, res) => {
-  Note.findById(req.params.id, (err, note) => {
-    if (err) throw err;
-
-  res.render('show-note', {note: note});
-  });  
-});
-
-app.post('/notes', (req, res) => {
-  Note.create(req.body, (err, note) => {
-    if (err) throw err;
-    res.redirect(`/notes/${note._id}`);
-  });
-  console.log(req.body);
-});
+app.use(note);
 
 mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
   if (err) throw err;
